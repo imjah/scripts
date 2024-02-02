c4746325f159de364e9c1e4967c2b0813c4ff149
imjah <imjah@tuta.io>
2024-02-02 23:17:31 +0100

    Fix tests

--- ttv/ttv_test.py
+++ ttv/ttv_test.py
@@ -2,10 +2,12 @@ import ttv
 
 def test_user_is_live():
     user = ttv.User('kapitanbombatv')
+    user.fetch()
 
     assert user.data['isLive']
 
 def test_user_is_not_live():
     user = ttv.User('imjah___')
+    user.fetch()
 
     assert user.data['isLive'] == False
