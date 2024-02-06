216c38ffe52e6e935abc2b2da18aa668e0742e26
imjah <imjah@tuta.io>
2024-02-06 22:33:10 +0100

    Add sorting by topic and viewers

--- ttv/ttv.py
+++ ttv/ttv.py
@@ -9,14 +9,25 @@ config = {
 
 class User:
     def __init__(self, name):
-        self.name = name
-        self.data = None
+        self.name    = name
+        self.live    = False
+        self.topic   = ''
+        self.viewers = 0
 
     def fetch(self):
         response = requests.get(f'{config["safetwitch_api"]}/users/{self.name}')
 
-        if response.status_code == 200:
-            self.data = response.json()['data']
+        if response.status_code != 200:
+            return
+
+        data = response.json()['data']
+
+        try:
+            self.live    = data['isLive']
+            self.topic   = data['stream']['topic']
+            self.viewers = data['stream']['viewers']
+        except KeyError:
+            return
 
 class Users:
     def __init__(self):
@@ -31,17 +42,24 @@ class Users:
         for thread in threads:
             thread.join()
 
+    def sort(self):
+        self.data.sort(key=lambda k: k.viewers, reverse=True)
+        self.data.sort(key=lambda k: k.topic)
+
+    def online(self):
+        return filter(lambda k: k.live, self.data)
+
     def _read_users(self):
         self.data = []
 
         with open(f'{config["dir"]}/channels') as file:
-            for line in sorted(file):
+            for line in file:
                 self.data.append(User(line.strip()))
 
 if __name__ == '__main__':
     users = Users()
     users.fetch()
+    users.sort()
 
-    for user in users.data:
-        if (user.data['isLive']):
-            print(f'{user.name} {user.data["stream"]["topic"]} {user.data["stream"]["viewers"]}')
+    for user in users.online():
+        print(f'{user.name} {user.topic} {user.viewers}')
